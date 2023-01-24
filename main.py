77038839951f365ac78d5e9bcf7dd9dce082d802
Tiago Peixoto <tspxttech@gmail.com>
2023-01-24 14:11:07 -0300

    iniciando a criação da classe ContaCorrente

---
+++ main.py
@@ -0,0 +1,15 @@
+#criação da classe ContaCorrente
+class ContaCorrente:
+
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+        self.saldo = 0
+
+
+#programa
+cc_fulano = ContaCorrente(nome='Fulano', cpf='999.888.777-66')
+
+
+
+
